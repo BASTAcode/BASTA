72c779922efc3e5678b241ec479dbba823845878
Jakob Lysgaard Rørsted <jakob@phys.au.dk>
2022-01-04 13:47:39 +0100

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -21,14 +21,14 @@ sys.path.insert(0, os.path.abspath("../examples"))
 
 # General information about the project.
 project = "BASTA"
-copyright = "2021, The BASTA Team"
+copyright = "2022, The BASTA Team"
 author = "The BASTA Team"
 
 # The short X.Y version.
-version = "1.1.2"
+version = "1.1.3"
 
 # The full version, including alpha/beta/rc tags.
-release = "1.1.2"
+release = "1.1.3"
 
 
 # -- General configuration ------------------------------------------------
