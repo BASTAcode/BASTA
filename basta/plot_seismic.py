Solar scaling, automatic dnu prior, interpolation fixes (#26)
* Remove plots warning from docs

* Add automatic prior on dnu based on dnufrac

* Activate solar scaling for dnu in freqfit

* Apply rescaling when printing BFM

* Update reference and docs plots

* Fix dnu xml write with interpolation

* Updated interpolation status check and compatibility with volume weight grids

* Finalised interpolation IntStatus implementation

* Interpolation volume weight compatibility implemented

* Minor plotting tweaks

* Fix dnu on echelle diagrams

---------

Co-authored-by: Mark Lykke Winther <mark@phys.au.dk>

basta/plot_seismic.py
@@ -889,7 +889,7 @@ def epsilon_difference_components_diagram(
     expol = np.where(np.logical_or(nu12 < min(nu0), nu12 > max(nu0)))[0]
 
     # Definition of figure
-    fig, ax = plt.subplots(2, 2, figsize=(11, 9), sharex="col", sharey="row")
+    fig, ax = plt.subplots(2, 2, figsize=(8.47, 6), sharex="col", sharey="row")
 
     # Epsilon differences
     for ll in l_available:
@@ -927,7 +927,7 @@ def epsilon_difference_components_diagram(
             obsepsdiff[1][indobs],
             obsepsdiff[0][indobs],
             yerr=obsepsdiff_err[indobs],
-            marker=obsmarker,
+            marker=modmarkers["l" + str(ll)],
             color=colors["l" + str(ll)],
             markeredgewidth=0.5,
             markeredgecolor="k",
@@ -966,7 +966,7 @@ def epsilon_difference_components_diagram(
             eps,
             yerr=err,
             linestyle=None,
-            fmt=obsmarker,
+            fmt=modmarkers["l" + str(ll)],
             color=colors["l" + str(ll)],
             zorder=3,
             label=elab % ll,

basta/utils_xml.py
@@ -75,6 +75,7 @@ def create_xmltag(
     star = SubElement(main, "star", {"starid": str(starid)})
 
     # Special treatment of dnu and numax for frequency fitting
+    # ++ Make sure to only add them once
     nuset = {"dnu": False, "numax": False}
 
     # Loop over fitting parameters
@@ -104,25 +105,9 @@ def create_xmltag(
         if not np.isclose(paramerr, missingval):
             SubElement(star, param, {"value": str(paramval)})
 
-    # Handle interpolation parameters
-    for param in intpollim:
-        out = {}
-        gparam = "dnu" if "dnu" in param else param
-        nucheck = not nuset[gparam] if gparam in nuset else False
-        if "abstol" in intpollim[param] or nucheck:
-            paramval = _get_param(paramvals, params, gparam)
-            if not np.isclose(paramval, missingval):
-                out["value"] = str(paramval)
-            if nucheck:
-                nuset[gparam] = True
-        if "sigmacut" in intpollim[param]:
-            paramerr = _get_param(paramvals, params, gparam + "_err")
-            if not np.isclose(paramerr, missingval):
-                out["error"] = str(paramerr)
-        SubElement(star, gparam, out)
-
-    # Handle the nottrustedfile object
+    # Handle the special things for frequency fitting
     if freqparams and any(x in fitparams for x in freqtypes.alltypes):
+        # The nottrustedfile object
         if "nottrustedfile" in freqparams:
             if isinstance(freqparams["nottrustedfile"], dict):
                 if starid in freqparams["nottrustedfile"].keys():
@@ -137,12 +122,40 @@ def create_xmltag(
                 )
             else:
                 raise ValueError("Nottrustedfile is neither a dict or a str")
+
+        # Always add dnu and numax for frequency fitting
         if not nuset["dnu"]:
             dnu = _get_param(paramvals, params, "dnu")
-            SubElement(star, "dnu", {"value": str(dnu)})
+            try:
+                dnu_err = _get_param(paramvals, params, "dnu_err")
+                SubElement(star, "dnu", {"value": str(dnu), "error": str(dnu_err)})
+            except IndexError:
+                SubElement(star, "dnu", {"value": str(dnu)})
+            nuset["dnu"] = True
+
         if not nuset["numax"]:
             numax = _get_param(paramvals, params, "numax")
             SubElement(star, "numax", {"value": str(numax)})
+            nuset["numax"] = True
+
+    # Handle interpolation parameters (special treatment of dnu; not add if added)
+    for param in intpollim:
+        print(param)
+        out = {}
+        gparam = "dnu" if "dnu" in param else param
+        nucheck = not nuset[gparam] if gparam in nuset else False
+        if "abstol" in intpollim[param] or nucheck:
+            paramval = _get_param(paramvals, params, gparam)
+            if not np.isclose(paramval, missingval):
+                out["value"] = str(paramval)
+            if nucheck:
+                nuset[gparam] = True
+        if "sigmacut" in intpollim[param]:
+            paramerr = _get_param(paramvals, params, gparam + "_err")
+            if not np.isclose(paramerr, missingval):
+                out["error"] = str(paramerr)
+        if not nuset[gparam]:
+            SubElement(star, gparam, out)
 
     return main
 

examples/preview_interpolation.py
@@ -219,7 +219,7 @@ def test_across_interpolation(
         sobol = resolution["scale"]
 
     if len(baseparams) == 0:
-        baseparams = [par.decode("UTF-8") for par in grid["header/active_weights"]]
+        baseparams = [par.decode("UTF-8") for par in grid["header/pars_sampled"]]
 
     base = np.zeros((len(selectedmodels), len(baseparams)))
     for i, name in enumerate(selectedmodels):