Fix scaling issue for dnuSer

src/basta/bastamain.py
@@ -237,7 +237,7 @@ def _bastamain(
             """
 
             # TODO(Amalie) Do this prior to the loop...
-            if "phase" in star.classicalparams.params.keys():
+            if star.phase is not None:
                 phaseindex = np.isin(libitem["phase"][:], iphases)
                 index &= phaseindex
 
@@ -320,7 +320,6 @@ def _bastamain(
 
                 # Add parameters not in fitparams
                 for param in list(allparams):
-                    print(param)
                     if param not in list(star.classicalparams.params.keys()):
                         paramvalues[param] = libitem[param][index]
 
@@ -386,7 +385,7 @@ def _bastamain(
                         raise ValueError(f"Unknown IMF: {inferencesettings.imf}")
                     val = imfs.PRIOR_FUNCTIONS[inferencesettings.imf](libitem, index)
                     logPDF += util.inflog(val)
-                    if debug:
+                    if outputoptions.debug:
                         IMFw += util.inflog(val)
 
                 # Calculate likelihood from weights, priors and chi2
@@ -416,11 +415,11 @@ def _bastamain(
                         index, logPDFarr, chi2
                     )
                 if (
-                    star.seismicparams.has_any_case
-                    and star.seismicparams.ratios.dnufit_in_ratios
+                    inferencesettings.has_any_seismic_case
+                    and inputstar.dnufit_in_ratios
                 ):
                     dnusurfmodels[group_name + name] = stats.Trackdnusurf(dnusurf)
-                if star.seismicparams.has_glitches:
+                if inferencesettings.has_glitches:
                     glitchmodels[group_name + name] = stats.Trackglitchpar(
                         glitchpar[:, 0],
                         glitchpar[:, 1],
@@ -479,12 +478,7 @@ def _bastamain(
         )
         return
 
-    # Print a header to signal the start of the output section in the log
-    print("\n*****************************************")
-    print("**                                     **")
-    print("**   Output and results from the fit   **")
-    print("**                                     **")
-    print("*****************************************\n")
+    util._banner("Output and results from the fit")
 
     # Find and print highest likelihood model info
     maxPDF_path, maxPDF_ind = stats.get_highest_likelihood(
@@ -513,7 +507,7 @@ def _bastamain(
         selectedmodels=selectedmodels,
         Grid=Grid,
         gridheader=gridheader,
-        absolutemagnitudes=absolutemagnitudes,
+        absolutemagnitudes=star.absolutemagnitudes,
         star=star,
         filepaths=filepaths,
         runfiles=runfiles,
@@ -530,7 +524,7 @@ def _bastamain(
     #    addstats["glitchparams"] = glitchmodels
 
     # Make frequency-related plots
-    if plotconfig.freqplots and star.seismicparams.has_any_case:
+    if plotconfig.freqplots and inferencesettings.has_any_seismic_case:
         plot_driver.plot_all_seismic(
             plotconfig,
             Grid=Grid,

src/basta/process_output.py
@@ -350,29 +350,19 @@ def compute_posterior(
         # Scale back to µHz before output/plot
         if param.startswith("dnu") and param not in ["dnufit", "dnufitMos12"]:
             scale = star.globalseismicparams.get_scale(param)
-            x *= inferencesettings.solarvalues["dnu"] / scale
+            x /= scale
             if param in fitparams:
-                fitparams_scaled[param] = fitparams[param].original
-                # (
-                #    np.asarray([fitparams[param]])
-                #    * inferencesettings.solarvalues["dnu"]
-                #    / scale
-                # )
+                fitparams_scaled[param] = fitparams[param].scaled
 
         elif param.startswith("numax"):
             x *= inferencesettings.solarvalues["numax"]
             if param in fitparams:
                 fitparams_scaled[param] = fitparams[param].original
-                # fitparams_scaled[param] = (
-                #    np.asarray([fitparams[param]])
-                #    * inferencesettings.solarvalues["numax"]
-                # )
         elif param in ["dnufit", "dnufitMos12"]:
             scale = star.globalseismicparams.get_scale(param)
             x /= scale
             if param in fitparams:
                 fitparams_scaled[param] = fitparams[param].original
-                # fitparams_scaled[param] = np.asarray([fitparams[param]]) / scale
 
         # Compute quantiles (using np.quantile is ~50 times faster than quantile_1D)
         xcen, xstdm, xstdp = stats.calc_key_stats(
@@ -566,6 +556,7 @@ def compute_posterior(
                 Grid=Grid,
                 selectedmodels=selectedmodels,
                 star=star,
+                inferencesettings=inferencesettings,
                 plotconfig=plotconfig,
                 absolutemagnitudes=absolutemagnitudes,
                 lp_interval=lp_interval,

src/basta/xml_run.py
@@ -775,11 +775,11 @@ def run_xml(
     useoptoutput = bool(optoutput)
 
     # Get priors
-    boxpriors: dict[str, Any] = {}
+    priors: dict[str, Any] = {}
     imf: str | None = None
     for param in root.findall("default/priors/"):
         if any(limit in param.attrib for limit in ["min", "max", "abstol", "sigmacut"]):
-            boxpriors[param.tag] = [
+            priors[param.tag] = [
                 float(param.attrib.get("min", -np.inf)),
                 float(param.attrib.get("max", np.inf)),
                 float(param.attrib.get("abstol", np.inf)),
@@ -999,7 +999,7 @@ def run_xml(
                 # inputparams["fitparams"] = starfitparams
                 inputparams["magnitudes"] = {}
                 inputparams["limits"] = {}
-                for param, (minval, maxval, abstol, nsigma) in boxpriors.items():
+                for param, (minval, maxval, abstol, nsigma) in priors.items():
                     if param in starfitparams:
                         val, err = starfitparams[param]
                         abstol = max(abstol, 2 * err * nsigma)