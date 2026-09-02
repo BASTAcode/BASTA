8e1364ea6fa2028e8d613bad55702b4bbb3ce701
Kuldeep Verma <kuldeepv89@gmail.com>
2026-09-02 08:22:36 +0530

    Bug fix for Gamma1 height and prior on tauhe and taucz

--- src/basta/glitch_fq.f95
+++ src/basta/glitch_fq.f95
@@ -116,7 +116,7 @@
         par(i0+5) = SQRT(par(i0+5)**2/(8.d0 * PI**2))
         par(i0+7) = MODULO(par(i0+7),2*PI)
         Hhe = par(i0+4)/par(i0+5)
-        Hhe = Hhe * acoustic_radius/SQRT(2.d0 * PI**3)
+        Hhe = Hhe * acoustic_radius * SQRT(2.d0 / PI)
 
         ! Update the fit only if:
         !--------------------------
@@ -127,7 +127,7 @@
         ! (3) maximum value of derivatives w.r.t. parameters is < 1d-2,
         ! (4) Acoustic width of He ionization zone > 0.1s,
         ! (5) acoustic depths of CZ and He > 0 s,
-        ! (6) acoustic depth of CZ > acoustic depth of He,
+        ! (6) acoustic depth of CZ > acoustic depth of He + 100 s,
         ! (7) acoustic depth of CZ < acoustic radius.
         reg_new = REGU_FQ(par)
         chi2_new = chi2_total_new - reg_new
@@ -136,7 +136,7 @@
             MAXVAL(ABS(grad(:))) .LT. tol_grad_fq .AND. &
             Hhe .LT. 1.67d0 .AND. &
             par(i0+2) .GT. 0.d0 .AND. par(i0+6) .GT. 0.d0 .AND. &
-            par(i0+2) .GT. par(i0+6) .AND. &
+            par(i0+2) .GT. (par(i0+6) + 100.d0) .AND. &
             par(i0+2) .LT. acoustic_radius) THEN
           chi2_total = chi2_total_new
           chi2  = chi2_new
